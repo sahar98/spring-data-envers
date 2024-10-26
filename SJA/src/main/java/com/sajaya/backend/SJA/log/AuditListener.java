e9f543a834f4f904cccc4f3e7c87833a08e81483
NAM <ahmadinaghedi.sahar@gmail.com>
2024-10-26 09:39:49 +0330

    chage pom

--- SJA/src/main/java/com/sajaya/backend/SJA/log/AuditListener.java
+++ SJA/src/main/java/com/sajaya/backend/SJA/log/AuditListener.java
@@ -1,24 +1,11 @@
 package com.sajaya.backend.SJA.log;
 
 import com.sajaya.backend.SJA.model.Publisher;
-import com.sajaya.backend.SJA.repository.PublisherRepository;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.PostUpdate;
 import jakarta.persistence.PreUpdate;
-import org.hibernate.envers.AuditReader;
-import org.hibernate.envers.AuditReaderFactory;
-import org.hibernate.envers.query.AuditEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.history.Revision;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Isolation;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
-import org.w3c.dom.events.Event;
-
-import java.util.Optional;
 
 @Component
 public class AuditListener
